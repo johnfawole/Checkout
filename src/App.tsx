7106551476fc9c5351f351f96ae40b4cc07d06e6
John Fáwọlé <johnfawole18@gmail.com>
2023-05-30 21:31:30 +0100

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -98,7 +98,7 @@ function App() {
 
       const options = {
         method: 'POST',
-        url: 'http://localhost:5000/api/verifyEmail',
+        url: 'https://verifyemailforpaper-production.up.railway.app/api/verifyEmail/',
         headers: {
           accept: 'application/json',
           'content-type': 'application/json',
