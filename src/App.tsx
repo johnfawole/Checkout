877fbc8df6e2780425867d3e2dcedf503cc945b0
John Fáwọlé <johnfawole18@gmail.com>
2023-05-30 19:14:41 +0100

    Made Changes

--- src/App.tsx
+++ src/App.tsx
@@ -98,7 +98,7 @@ function App() {
 
       const options = {
         method: 'POST',
-        url: 'http://localhost:5000/api/verifyEmail',
+        url: 'https://verify-email-for-paper.vercel.app/api/verifyEmail',
         headers: {
           accept: 'application/json',
           'content-type': 'application/json',
